7f342d2521aa2697babcb478e57c294a3f33ad5f
JeremyHudson43 <jer4349@gmail.com>
2022-04-01 19:06:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,46 +3,49 @@ import pyttsx3
 import wikipedia
 from moviepy.editor import concatenate_audioclips, AudioFileClip
 from pydub import AudioSegment
+import time
 
-articles_to_get = open("people_to_transcribe.txt", "r").readlines()
+articles_to_get = open("stuff_to_transcribe.txt", "r").readlines()
 
 engine = pyttsx3.init()
-
 engine.setProperty("rate", 400)
 
 volume = engine.getProperty('volume')
-engine.setProperty('volume', 0.01)
+engine.setProperty('volume', 0.5)
 
 
 def scrape_wikipedia(articles_to_get):
+    counter = 1
+
     for article in articles_to_get:
-
         try:
+            if not os.path.exists(f'./output_mp3/{counter}_{article}.mp3'):
+                article = article.strip()
 
-            article = article.strip()
+                if 'Level 2' in article:
+                    article = article.replace('(Level 2)', '')
+                elif 'Level 1' in article:
+                    article = article.replace('(Level 1)', '')
 
-            p = wikipedia.page(article, auto_suggest=False)
-            content = p.content.split('== See also ==')[0]
+                p = wikipedia.page(article, auto_suggest=False)
 
-            engine.save_to_file(content, f'./output_mp3/{article}.mp3')
+                content = p.content.split('== See also ==')[0]
 
-            # run and wait method, it processes the voice commands.
-            engine.runAndWait()
+                engine.save_to_file(content, f'./output_mp3/{counter}_{article}.mp3')
 
-            sound = AudioSegment.from_file(f'./output_mp3/{article}.mp3')
-            sound.export(f"./output_mp3/{article}.mp3", format="mp3", bitrate="128k")
+                # run and wait method, it processes the voice commands.
+                engine.runAndWait()
+
+                sound = AudioSegment.from_file(f"./output_mp3/{counter}_{article}.mp3")
+                sound.export(f"./output_mp3/{counter}_{article}.mp3", format="mp3", bitrate="64k")
+
+                counter = counter + 1
+
+                if counter % 10 == 0:
+                    time.sleep(3)
 
         except Exception as err:
             print(err)
 
 
-def concatenate_audio_moviepy(audio_clip_paths, output_path):
-    clips = [AudioFileClip(os.path.join(output_path, c)) for c in audio_clip_paths]
-    final_clip = concatenate_audioclips(clips)
-    final_clip.write_audiofile(os.path.join(output_path, 'combined.mp3'))
-
-
-all_files = os.listdir('./output_mp3')
-
 scrape_wikipedia(articles_to_get)
-concatenate_audio_moviepy(all_files, './output_mp3')
